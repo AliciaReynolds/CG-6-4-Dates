c86f7767d6dbba77de757355af0ff12744df8ebe
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-18 19:40:24 -0500

    Inital Code

--- CG 6-4 Dates/Program.cs
+++ CG 6-4 Dates/Program.cs
@@ -6,7 +6,20 @@ namespace CG_6_4_Dates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's find the difference between your birthday and today.");
+
+            Console.WriteLine("Please input your birthday in the following format - mm/dd/yyyy.");
+
+            DateTime Birthday = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input today's date in the following format - mm/dd/yyyy.");
+            DateTime Today = DateTime.Parse(Console.ReadLine());
+
+            TimeSpan DateDifference = DateTime.Today.Subtract(Birthday);
+            Console.WriteLine(DateDifference.TotalDays);
+            Console.ReadLine();
+
+
         }
     }
 }
