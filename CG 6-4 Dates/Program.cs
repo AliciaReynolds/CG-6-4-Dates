204be93e9951714cc0d71bc739b2ffddaf7c05a8
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-18 19:46:43 -0500

    Finish code and notes

--- CG 6-4 Dates/Program.cs
+++ CG 6-4 Dates/Program.cs
@@ -6,17 +6,21 @@ namespace CG_6_4_Dates
     {
         static void Main(string[] args)
         {
+            //This code asks for the user to input a date
             Console.WriteLine("Let's find the difference between your birthday and today.");
-
             Console.WriteLine("Please input your birthday in the following format - mm/dd/yyyy.");
 
+            //This code takes the inputted data and assigns it to a variable
             DateTime Birthday = DateTime.Parse(Console.ReadLine());
 
+            //This code asks the user to input today's date, then stores that info into a variable
             Console.WriteLine("Please input today's date in the following format - mm/dd/yyyy.");
             DateTime Today = DateTime.Parse(Console.ReadLine());
 
+            //This code takes the two dates (today's date and the birthday) that the user
+            //provides, subtracts them, and then gives the difference in days.
             TimeSpan DateDifference = DateTime.Today.Subtract(Birthday);
-            Console.WriteLine(DateDifference.TotalDays);
+            Console.WriteLine(DateDifference.TotalDays + " days");
             Console.ReadLine();
 
 
